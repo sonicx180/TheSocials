01c05691069b350566f6b8d5d3f828189fb97fce
Sonicx180 <83236320+sonicx180@users.noreply.github.com>
2025-05-15 04:51:42 +0000

    some changes

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<div class="sidebar">
+	<nav>
+		<h1>The Social</h1>
+		<h2 class="sidebar-links"><a href="/feed">Feed </a></h2>
+		<h2 class="sidebar-links"><a href="/my-posts">Your Posts </a></h2>
+		<!-- make this one reactive-->
+		<h2 class="sidebar-links"><a href="/login">Login </a></h2>
+	</nav>
+
+	<footer>
+		<h3><a href="/contact">Contact </a></h3>
+		<h3><a href="/terms">Terms of Service </a></h3>
+	</footer>
+</div>
+
+<style>
+    .sidebar {
+        background-color:var(--bold-blue);
+        
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,14 @@
-<script lang = "ts">
-    
+<script lang="ts">
+	import Sidebar from '../components/Sidebar.svelte';
+	import '../app.css';
 </script>
+
+<svelte:head>
+	<title>The Social!</title>
+	<meta
+		name="description"
+		content="The Social is a social media website, designed to be for people to share their thoughts and ideas."
+	/>
+</svelte:head>
+<Sidebar />
+<slot />
